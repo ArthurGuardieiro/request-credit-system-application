864ec2c07a07690e9a1785e038fa3f8a22208749
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2023-05-28 16:32:46 -0300

    refatorando para testar API com Postman

--- src/main/kotlin/me/dio1/request/credit/system/controller/CreditResource.kt
+++ src/main/kotlin/me/dio1/request/credit/system/controller/CreditResource.kt
@@ -38,7 +38,7 @@ class CreditResource(
             .collect(Collectors.toList()))
     }
 
-    @GetMapping("/creditCode")
+    @GetMapping("/{creditCode}")
     fun findByCreditCode(
         @RequestParam(value = "customerId") customerId: Long,
         @PathVariable creditCode: UUID
